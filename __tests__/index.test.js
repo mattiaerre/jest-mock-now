92e7fc5dff10221c887f065c5b5309716183439d
mattia richetto <mattia.richetto@gmail.com>
2017-06-23 10:11:03 -0700

    mock Date.now()

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,5 +1,17 @@
 const index = require('../');
 
-test('oh noes!', () => {
-  expect(() => { index(); }).toThrowErrorMatchingSnapshot();
+test('Date.now() to match snapshot w/o options', () => {
+  const _ = 1479427200000;
+  index();
+
+  expect(Date.now()).toEqual(_);
+  expect(Date.now()).toMatchSnapshot();
+});
+
+test('Date.now() to match snapshot w/ options', () => {
+  const now = new Date('2017-06-22');
+  index(now);
+
+  expect(Date.now()).toEqual(now.getTime());
+  expect(Date.now()).toMatchSnapshot();
 });
--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-module.exports = () => { throw new Error('oh noes!'); };
+module.exports = (date) => {
+  const _ = 1479427200000; // 2016-11-18 00:00:00.000;
+  const now = date ? date.getTime() : _;
+  Date.now = jest.fn(() => now);
+};
