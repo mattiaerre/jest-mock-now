1faa21a38130452f1a33306a8fa3309a3e28100f
mattia richetto <mattia.richetto@gmail.com>
2017-06-24 19:52:58 -0700

    remove line

--- index.js
+++ index.js
@@ -2,6 +2,5 @@ const { NOW } = require('./constants');
 
 module.exports = date => {
   const now = date ? date.getTime() : NOW;
-
   Date.now = jest.spyOn(Date, 'now').mockImplementation(() => now);
 };
